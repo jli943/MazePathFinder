91b7649fedd7543ac11738995455d8cf5594e78a
JL <ljw1203@icloud.com>
2023-07-27 13:58:31 -0400

    Initial Commit

---
+++ Pathfinding Visualizer/Pathfinding_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pathfinding_VisualizerApp.swift
+//  Pathfinding Visualizer
+//
+//  Created by Junwei Li on 2023/7/27.
+//
+
+import SwiftUI
+
+@main
+struct Pathfinding_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
